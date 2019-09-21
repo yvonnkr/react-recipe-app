b7fc2fcbc384617e7c4473d07b2c5e38e6f7769d
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-21 22:46:58 +0100

    Setup Header Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>hello from home page</h1>
+        <Header title='amazing recipes'>
+          <Link
+            to='/recipes'
+            className='text-uppercase btn btn-secondary btn-lg mt-3'
+          >
+            search recipes
+          </Link>
+        </Header>
       </div>
     );
   }
