300999261cf743ca209e78e685ef3ed1f7508297
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-22 11:58:28 +0100

    Setup Default 404 page

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h4>hello from default (404 / error) page</h4>
-      </div>
+      <Header title='404' styleClass='default-hero'>
+        <h2 className='text-white text-uppercase'>Page Not Found</h2>
+        <Link to='/' className='text-uppercase btn btn-secondary btn-lg mt-3'>
+          Return Home
+        </Link>
+      </Header>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 export default class Home extends Component {
   render() {
