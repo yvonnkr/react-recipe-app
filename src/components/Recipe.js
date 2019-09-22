9fbf868ef017ccd5180cc9e97197475bf53605f7
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-22 16:09:30 +0100

    Wiredup Recipe List and Recipes Page

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Recipe extends Component {
   render() {
+    const {
+      publisher,
+      title,
+      source_url,
+      recipe_id,
+      image_url
+    } = this.props.recipe;
+
     return (
-      <div>
-        <h2>Recipe</h2>
+      <div className='col-10 mx-auto col-md-6 col-lg-4 my-3'>
+        <div className='card' style={{ height: '100%' }}>
+          <img
+            src={image_url}
+            alt='recipe'
+            style={{ height: '14rem' }}
+            className='img-card-top'
+          />
+          <div className='card-body text-capitalize'>
+            <h6>{title}</h6>
+            <h6 className='text-warning text-slanted'>
+              provided by {publisher}
+            </h6>
+          </div>
+          <div className='card-footer'>
+            <Link to={`/recipes/${recipe_id}`} className="btn btn-primary text-capitalize" >Details</Link>
+            <a href={source_url} target='_blank' rel='noopener noreferrer' className="btn btn-success mx-2 text-capitalize" >
+              Recipe Url
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
